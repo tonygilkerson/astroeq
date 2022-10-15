f841da24dc7397a755865730c1f43180c3ff4a59
Tony Gilkerson <tgilkerson@miletwo.us>
2022-10-15 11:57:25 -0400

    hid is working

--- cmd/handset/main.go
+++ cmd/handset/main.go
@@ -2,11 +2,89 @@ package main
 
 import (
 	"fmt"
+	"machine"
+	"time"
+
 	"github.com/tonygilkerson/astroeq/pkg/hid"
 )
 
 func main() {
-	fmt.Println("handset")
-	foo := hid.Foo()
-	fmt.Println(foo)
+
+	// run light
+	runLight()
+
+	//
+	// keypad keys
+	//
+	zeroKey := machine.GP3
+	oneKey := machine.GP11
+	twoKey := machine.GP12
+	threeKey := machine.GP13
+	fourKey := machine.GP8
+	fiveKey := machine.GP9
+	sixKey := machine.GP10
+	sevenKey := machine.GP5
+	eightKey := machine.GP6
+	nineKey := machine.GP7
+
+	scrollUpKey := machine.GP4
+	scrollDnKey := machine.GP2
+
+	rightKey := machine.GP14
+	leftKey := machine.GP15
+	upKey := machine.GP16
+	downKey := machine.GP17
+
+	escKey := machine.GP18
+	setupKey := machine.GP19
+	enterKey := machine.GP20
+
+	handset, _ := hid.NewHandset(
+		zeroKey,
+		oneKey,
+		twoKey,
+		threeKey,
+		fourKey,
+		fiveKey,
+		sixKey,
+		sevenKey,
+		eightKey,
+		nineKey,
+		scrollUpKey,
+		scrollDnKey,
+		rightKey,
+		leftKey,
+		upKey,
+		downKey,
+		escKey,
+		setupKey,
+		enterKey)
+
+	keyStrokes := handset.Configure()
+
+	//
+	// Capture key strokes
+	//
+	for k := range keyStrokes {
+		keyName := handset.GetKeyName(k)
+
+		fmt.Printf("[main] KeyName: %s\n", keyName)
+	}
+
+}
+
+func runLight() {
+
+	// run light
+	led := machine.LED
+	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+
+	// blink run light for a bit seconds so I can tell it is starting
+	for i := 0; i < 25; i++ {
+		led.High()
+		time.Sleep(time.Millisecond * 100)
+		led.Low()
+		time.Sleep(time.Millisecond * 100)
+	}
+	led.High()
 }
--- pkg/hid/hid.go
+++ pkg/hid/hid.go
@@ -1,11 +1,45 @@
 package hid
 
 import (
+	// "fmt"
 	"machine"
+	"time"
+)
+
+type Key uint8
+
+const (
+	UndefinedKey Key = iota
+	ZeroKey
+	OneKey
+	TwoKey
+	ThreeKey
+	FourKey
+	FiveKey
+	SixKey
+	SevenKey
+	EightKey
+	NineKey
+
+	ScrollDnKey
+	ScrollUpKey
+
+	RightKey
+	LeftKey
+	UpKey
+	DownKey
+
+	EscKey
+	SetupKey
+	EnterKey
 )
 
 // If any key is pressed record the corresponding pin
-var keyPressed machine.Pin
+var keyPressed Key
+
+// As keys are pressed they are published to the keyStrokes chan
+// a buffer of 100 ensure key stroke publishing is not blocked
+var keyStrokes = make(chan Key, 100)
 
 type Handset struct {
 	scrollDnKey machine.Pin
@@ -36,28 +70,26 @@ type Handset struct {
 
 // Returns a new Handset
 func NewHandset(
-	scrollDnKey machine.Pin,
-	zeroKey     machine.Pin,
-	scrollUpKey machine.Pin,
-
-	sevenKey machine.Pin,
-	eightKey machine.Pin,
-	nineKey  machine.Pin,
-
+	zeroKey machine.Pin,
+	oneKey machine.Pin,
+	twoKey machine.Pin,
+	threeKey machine.Pin,
 	fourKey machine.Pin,
 	fiveKey machine.Pin,
-	sixKey  machine.Pin,
-
-	oneKey   machine.Pin,
-	twoKey   machine.Pin,
-	threeKey machine.Pin,
+	sixKey machine.Pin,
+	sevenKey machine.Pin,
+	eightKey machine.Pin,
+	nineKey machine.Pin,
+	
+	scrollDnKey machine.Pin,
+	scrollUpKey machine.Pin,
 
 	rightKey machine.Pin,
-	leftKey  machine.Pin,
-	upKey    machine.Pin,
-	downKey  machine.Pin,
+	leftKey machine.Pin,
+	upKey machine.Pin,
+	downKey machine.Pin,
 
-	escKey   machine.Pin,
+	escKey machine.Pin,
 	setupKey machine.Pin,
 	enterKey machine.Pin,
 ) (Handset, error) {
@@ -84,7 +116,10 @@ func NewHandset(
 	}, nil
 }
 
-func (hs *Handset) Configure() {
+// Configure - will configure the HID pins and assign each to a key
+// starts a go routine to listen for key strokes and publishes each to the key chan
+// the key channel is returned for key stroke subscribers
+func (hs *Handset) Configure() chan Key {
 	hs.scrollDnKey.Configure(machine.PinConfig{Mode: machine.PinInputPulldown})
 	hs.zeroKey.Configure(machine.PinConfig{Mode: machine.PinInputPulldown})
 	hs.scrollUpKey.Configure(machine.PinConfig{Mode: machine.PinInputPulldown})
@@ -105,23 +140,101 @@ func (hs *Handset) Configure() {
 	hs.setupKey.Configure(machine.PinConfig{Mode: machine.PinInputPulldown})
 	hs.enterKey.Configure(machine.PinConfig{Mode: machine.PinInputPulldown})
 
-	hs.scrollDnKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = p })
-	hs.zeroKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = p })
-	hs.scrollUpKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = p })
-	hs.sevenKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = p })
-	hs.eightKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = p })
-	hs.nineKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = p })
-	hs.fourKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = p })
-	hs.fiveKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = p })
-	hs.sixKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = p })
-	hs.oneKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = p })
-	hs.twoKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = p })
-	hs.threeKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = p })
-	hs.rightKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = p })
-	hs.leftKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = p })
-	hs.upKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = p })
-	hs.downKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = p })
-	hs.escKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = p })
-	hs.setupKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = p })
-	hs.enterKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = p })
+	hs.zeroKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = ZeroKey })
+	hs.oneKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = OneKey })
+	hs.twoKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = TwoKey })
+	hs.threeKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = ThreeKey })
+	hs.fourKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = FourKey })
+	hs.fiveKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = FiveKey })
+	hs.sixKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = SixKey })
+	hs.sevenKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = SevenKey })
+	hs.eightKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = EightKey })
+	hs.nineKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = NineKey })
+	hs.scrollUpKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = ScrollUpKey })
+	hs.scrollDnKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = ScrollDnKey })
+	hs.rightKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = RightKey })
+	hs.leftKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = LeftKey })
+	hs.upKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = UpKey })
+	hs.downKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = DownKey })
+	hs.escKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = EscKey })
+	hs.setupKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = SetupKey })
+	hs.enterKey.SetInterrupt(machine.PinFalling, func(p machine.Pin) { keyPressed = EnterKey })
+
+	// Start go routine that will listen for key strokes and publish them on a channel
+	go publishKeys()
+
+	return keyStrokes
+
 }
+
+// PublishKeys will capture the keys pressed and publish them to the keyStrokes channel
+func publishKeys() {
+	for {
+
+		// If any key was pressed
+		if keyPressed != UndefinedKey {
+
+			//
+			//  After a small delay if the key pressed has not changed, consider it "pressed"
+			//
+			key := keyPressed
+			time.Sleep(time.Millisecond * 100)
+			// fmt.Printf("[publishKeys] value: %v \n", key)
+
+			if key == keyPressed {
+				keyStrokes <- keyPressed
+				keyPressed = UndefinedKey //reset for next key press
+			}
+		}
+		time.Sleep(time.Millisecond * 500)
+	}
+
+}
+
+func (hs *Handset) GetKeyName(k Key) string {
+
+	// fmt.Printf("[GetKeyName] value: %v \n", k)
+	switch k {
+	case ZeroKey:
+		return "0"
+	case OneKey:
+		return "1"
+	case TwoKey:
+		return "2"
+	case ThreeKey:
+		return "3"
+	case FourKey:
+		return "4"
+	case FiveKey:
+		return "5"
+	case SixKey:
+		return "6"
+	case SevenKey:
+		return "7"
+	case EightKey:
+		return "8"
+	case NineKey:
+		return "9"
+	case ScrollDnKey:
+		return "ScrollDn"
+	case ScrollUpKey:
+		return "ScrollUp"
+	case RightKey:
+		return "Right"
+	case LeftKey:
+		return "Left"
+	case UpKey:
+		return "Up"
+	case DownKey:
+		return "Down"
+	case EscKey:
+		return "ESC"
+	case SetupKey:
+		return "Setup"
+	case EnterKey:
+		return "Enter"
+	default:
+		return "Unknown"
+	}
+}
+
