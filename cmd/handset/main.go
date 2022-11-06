4d5bf0334710b6a1ce2297bf7da7cbd02b073799
Tony Gilkerson <tgilkerson@miletwo.us>
2022-11-05 22:55:35 -0400

    added display

--- cmd/handset/main.go
+++ cmd/handset/main.go
@@ -4,14 +4,70 @@ import (
 	"fmt"
 	"machine"
 	"time"
-
+	"image/color"
 	"github.com/tonygilkerson/astroeq/pkg/hid"
+
+	"tinygo.org/x/drivers/ssd1351"
+	"tinygo.org/x/tinyfont"
+	"tinygo.org/x/tinyfont/freemono"	
 )
 
 func main() {
 
 	// run light
 	runLight()
+
+	//
+	// SPI for Display
+	//
+	machine.SPI0.Configure(machine.SPIConfig{
+		Frequency: 2000000,
+		LSBFirst:  false,
+		Mode:      0,
+		DataBits:  8,
+		SCK:       machine.SPI0_SCK_PIN, // GP18
+		SDO:       machine.SPI0_SDO_PIN, // GP19
+		SDI:       machine.SPI0_SDI_PIN, // GP16
+	})
+
+	var rst machine.Pin // ran out of pins
+	dc := machine.Pin(28)
+	cs := machine.Pin(27)
+	var en machine.Pin // ran out of pins
+	var rw machine.Pin // ran out of pins
+
+	display := ssd1351.New(machine.SPI0, rst, dc, cs, en, rw)
+
+	display.Configure(ssd1351.Config{
+		Width:        128,
+		Height:       128,
+		RowOffset:    0,
+		ColumnOffset: 0,
+	})
+
+	// not sure if this is needed
+	display.Command(ssd1351.SET_REMAP_COLORDEPTH)
+	display.Data(0x62)
+
+	display.FillScreen(color.RGBA{0, 0, 0, 0})
+	red := color.RGBA{0, 0, 255, 255}
+
+
+	tinyfont.WriteLine(&display, &freemono.Regular12pt7b, 3, 15, "ESC = clr", red)
+	display.FillRectangle(3,20,125,1,red)
+
+	tinyfont.WriteLine(&display, &freemono.Regular12pt7b, 3, 40, "Test 0001", red)
+	display.FillRectangle(3,45,124,1,red)
+
+	tinyfont.WriteLine(&display, &freemono.Regular12pt7b, 3, 65, "Test 0002", red)
+	display.FillRectangle(3,70,123,1,red)
+
+	tinyfont.WriteLine(&display, &freemono.Regular12pt7b, 3, 90, "Test 0003", red)
+	display.FillRectangle(3,70,123,1,red)
+	
+	tinyfont.WriteLine(&display, &freemono.Regular12pt7b, 3, 115, "Test 0004", red)
+	display.FillRectangle(3,70,123,1,red)
+
 
 	//
 	// keypad keys
@@ -32,11 +88,11 @@ func main() {
 
 	rightKey := machine.GP14
 	leftKey := machine.GP15
-	upKey := machine.GP16
+	upKey := machine.GP21
 	downKey := machine.GP17
 
-	escKey := machine.GP18
-	setupKey := machine.GP19
+	escKey := machine.GP22
+	setupKey := machine.GP26
 	enterKey := machine.GP20
 
 	handset, _ := hid.NewHandset(
@@ -65,10 +121,24 @@ func main() {
 	//
 	// Capture key strokes
 	//
+	
+	dsp := ""
+
 	for k := range keyStrokes {
 		keyName := handset.GetKeyName(k)
-
 		fmt.Printf("[main] KeyName: %s\n", keyName)
+		
+		switch k {
+		case hid.EscKey:
+			display.FillScreen(color.RGBA{0, 0, 0, 0}) // Clear screen
+			dsp = ""
+		case hid.EnterKey:
+			dsp = dsp + "\n"
+		default:
+			dsp = dsp + keyName + " "
+		}
+
+		tinyfont.WriteLine(&display, &freemono.Regular12pt7b, 3, 15, dsp, red)
 	}
 
 }
@@ -80,7 +150,7 @@ func runLight() {
 	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
 
 	// blink run light for a bit seconds so I can tell it is starting
-	for i := 0; i < 25; i++ {
+	for i := 0; i < 10; i++ {
 		led.High()
 		time.Sleep(time.Millisecond * 100)
 		led.Low()
